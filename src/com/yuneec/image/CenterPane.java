b1aafb19af57a2053ad1d7ea3810300ddd630624
fei.li <fei.li@yuneec.com>
2021-05-10 10:50:20 +0800

    add Clear temperature information function.

--- src/com/yuneec/image/CenterPane.java
+++ src/com/yuneec/image/CenterPane.java
@@ -1,5 +1,6 @@
 package com.yuneec.image;
 
+import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
@@ -14,6 +15,8 @@ import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class CenterPane {
 
@@ -21,6 +24,14 @@ public class CenterPane {
     public Pane centerSettingPane;
     public Pane centerImagePane;
     public Pane showImagePane; //640*512
+
+    public enum CenterSettingSelect {
+        NONE,
+        CLEAR,
+        POINT,
+        BOX,
+        ColorPalette
+    }
 
     private int showImagePaneX, showImagePaneY;
     private float pointTemperature;
@@ -32,16 +43,18 @@ public class CenterPane {
     private int endLineY = 0;
     private Line topLine, bottomLine, leftLine, rightLine;
 
-    private int centerSettingFlag ;
+    private CenterSettingSelect centerSettingFlag = CenterSettingSelect.NONE;
     private Pane centerSettingColorPalettePane;
     private boolean centerSettingColorPalettePaneAdded = false;
     private Background centerSettingButtonUnclickBackground;
     private Background centerSettingButtonClickBackground;
-    private Button SingleClickButton,BoxChooseButton,ColorPaletteButton;
+    private Button SingleClickButton,BoxChooseButton,ColorPaletteButton,ClearButton;
+    private ArrayList centerSettingButtonNodeList = new ArrayList();
 
-    private ArrayList oneTemperatureDrawMax = new ArrayList();
-    private ArrayList oneTemperatureDrawMin = new ArrayList();
-    private ArrayList oneTemperatureDraw;
+    private ArrayList boxTemperatureNodeMax = new ArrayList();
+    private ArrayList boxTemperatureNodeMin = new ArrayList();
+    private ArrayList onePointTemperatureNode;
+    private ArrayList pointTemperatureNodeList = new ArrayList();
 
     public ArrayList<String[]> pointTemperatureDataList = new ArrayList<>();
     public String[] boxTemperatureData = null;
@@ -67,13 +80,9 @@ public class CenterPane {
     }
 
     public void showImage() {
+        resetShowImageDefault();
         ImageUtil.readImage(Global.currentOpenImagePath);
         RightPane.getInstance().showImageInfoToRightPane();
-        resetCenterSetting();
-        centerSettingColorPalettePaneAdded = false;
-        showImagePane.getChildren().clear();
-        pointTemperatureDataList.clear();
-        boxTemperatureData = null;
 
         Image image = new Image("file:" + Global.currentOpenImagePath);
         imageView = new ImageView(image);
@@ -100,8 +109,9 @@ public class CenterPane {
             public void handle(MouseEvent e) {
 //				String s = "x=" + (int) e.getX() + " y=" + (int) e.getY();
 //				System.out.println("MouseClicked:" + s);
-                if(centerSettingFlag == 1 && Utils.mouseLeftClick(e)){
-                    addLabelInImage((int) e.getX(), (int) e.getY(),pointTemperature,Configs.white_color);
+                if(centerSettingFlag == CenterSettingSelect.POINT && Utils.mouseLeftClick(e)){
+                    ArrayList pointNodeList = addLabelInImage((int) e.getX(), (int) e.getY(),pointTemperature,Configs.white_color);
+                    pointTemperatureNodeList.add(pointNodeList);
                     String[] pointTemperatureData = new String[]{String.valueOf((int) e.getX()), String.valueOf((int) e.getY()), getFormatTemperature(pointTemperature)};
                     pointTemperatureDataList.add(pointTemperatureData);
                 }
@@ -111,10 +121,10 @@ public class CenterPane {
         imageView.setOnMouseDragged(event->{
 //			String s = "x=" + (int) event.getX() + " y=" + (int) event.getY();
 //			System.out.println("setOnMouseDragged:" + s);
-            if(centerSettingFlag == 2){
+            if(centerSettingFlag == CenterSettingSelect.BOX){
                 addRectangleForImage((int) event.getX(),(int) event.getY());
-                showImagePane.getChildren().removeAll(oneTemperatureDrawMax);
-                showImagePane.getChildren().removeAll(oneTemperatureDrawMin);
+                showImagePane.getChildren().removeAll(boxTemperatureNodeMax);
+                showImagePane.getChildren().removeAll(boxTemperatureNodeMin);
             }
         });
 
@@ -131,12 +141,12 @@ public class CenterPane {
             if (startLineX == (int) event.getX() && startLineY == (int) event.getY()){
                 return;
             }
-            if(centerSettingFlag == 2){
+            if(centerSettingFlag == CenterSettingSelect.BOX){
                 BoxTemperatureUtil.getInstance().init(startLineX, startLineY, endLineX, endLineY, new BoxTemperatureUtil.MaxMinTemperature() {
                     @Override
                     public void onResust(float maxTemperature, int[] maxTemperatureXY, float minTemperature, int[] minTemperatureXY) {
-                        oneTemperatureDrawMax = addLabelInImage(maxTemperatureXY[0],maxTemperatureXY[1],maxTemperature,Configs.red_color);
-                        oneTemperatureDrawMin = addLabelInImage(minTemperatureXY[0],minTemperatureXY[1],minTemperature,Configs.blue2_color);
+                        boxTemperatureNodeMax = addLabelInImage(maxTemperatureXY[0],maxTemperatureXY[1],maxTemperature,Configs.red_color);
+                        boxTemperatureNodeMin = addLabelInImage(minTemperatureXY[0],minTemperatureXY[1],minTemperature,Configs.blue2_color);
                         boxTemperatureData = new String[]{String.valueOf(startLineX), String.valueOf(startLineY),
                                 String.valueOf(endLineX), String.valueOf(endLineY),
                                 getFormatTemperature(maxTemperature), getFormatTemperature(minTemperature)};
@@ -181,7 +191,7 @@ public class CenterPane {
     }
 
     private ArrayList addLabelInImage(int x, int y ,float temperature,String color) {
-        oneTemperatureDraw = new ArrayList();
+        onePointTemperatureNode = new ArrayList();
         Label label = new Label();
 //        int num = new Random().nextInt(100) - 50;
         label.setText(getFormatTemperature(temperature));
@@ -200,11 +210,11 @@ public class CenterPane {
         Line xLine = drawLine(x-lineLEN,y,x+lineLEN,y,color);
         Line yLine = drawLine(x,y-lineLEN,x,y+lineLEN,color);
         showImagePane.getChildren().addAll(xLine,yLine);
-        oneTemperatureDraw.add(label);
-        oneTemperatureDraw.add(circle);
-        oneTemperatureDraw.add(xLine);
-        oneTemperatureDraw.add(yLine);
-        return oneTemperatureDraw;
+        onePointTemperatureNode.add(label);
+        onePointTemperatureNode.add(circle);
+        onePointTemperatureNode.add(xLine);
+        onePointTemperatureNode.add(yLine);
+        return onePointTemperatureNode;
     }
 
     public String getFormatTemperature(float temperature){
@@ -252,15 +262,16 @@ public class CenterPane {
         ColorPaletteButton = creatSettingButton("image/color_palette.png",null);
         ColorPaletteButton.setTranslateX(40);
 
+        ClearButton = creatSettingButton("image/clear.png",null);
+        ClearButton.setTranslateX(50);
+
         SingleClickButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent e) {
                 if (Utils.mouseLeftClick(e)) {
 //                    System.out.println("initCenterSettingPane SingleClickButton MouseClicked ...");
-                    SingleClickButton.setBackground(centerSettingButtonClickBackground);
-                    BoxChooseButton.setBackground(centerSettingButtonUnclickBackground);
-                    ColorPaletteButton.setBackground(centerSettingButtonUnclickBackground);
-                    centerSettingFlag = 1;
+                    setButtonClickBackground(centerSettingButtonNodeList,SingleClickButton);
+                    centerSettingFlag = CenterSettingSelect.POINT;
                     dmissColorPalettePane();
                 }
             }
@@ -271,10 +282,8 @@ public class CenterPane {
             public void handle(MouseEvent e) {
                 if (Utils.mouseLeftClick(e)) {
 //                    System.out.println("initCenterSettingPane BoxChooseButton MouseClicked ...");
-                    BoxChooseButton.setBackground(centerSettingButtonClickBackground);
-                    SingleClickButton.setBackground(centerSettingButtonUnclickBackground);
-                    ColorPaletteButton.setBackground(centerSettingButtonUnclickBackground);
-                    centerSettingFlag = 2;
+                    setButtonClickBackground(centerSettingButtonNodeList,BoxChooseButton);
+                    centerSettingFlag = CenterSettingSelect.BOX;
                     dmissColorPalettePane();
                 }
             }
@@ -285,9 +294,8 @@ public class CenterPane {
             public void handle(MouseEvent e) {
                 if (Utils.mouseLeftClick(e)) {
 //                    System.out.println("initCenterSettingPane ColorPaletteButton MouseClicked ...");
-                    SingleClickButton.setBackground(centerSettingButtonUnclickBackground);
-                    BoxChooseButton.setBackground(centerSettingButtonUnclickBackground);
-                    centerSettingFlag = 3;
+                    setButtonClickBackground(centerSettingButtonNodeList,ColorPaletteButton);
+                    centerSettingFlag = CenterSettingSelect.ColorPalette;
                     if(centerSettingColorPalettePaneAdded){
                         dmissColorPalettePane();
                         ColorPaletteButton.setBackground(centerSettingButtonUnclickBackground);
@@ -299,9 +307,38 @@ public class CenterPane {
             }
         });
 
+        ClearButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent e) {
+                if (Utils.mouseLeftClick(e)) {
+                    centerSettingFlag = CenterSettingSelect.CLEAR;
+                    setButtonClickBackground(centerSettingButtonNodeList,ClearButton);
+                    TimerTask task= new TimerTask() {
+                        @Override
+                        public void run() {
+                            Platform.runLater(new Runnable() {
+                                @Override
+                                public void run() {
+                                    resetShowImageDefault();
+                                }
+                            });
+                        }
+                    };
+                    Timer timer=new Timer();
+                    timer.schedule(task,120);
+                }
+            }
+        });
+
+        centerSettingButtonNodeList.clear();
+        centerSettingButtonNodeList.add(SingleClickButton);
+        centerSettingButtonNodeList.add(BoxChooseButton);
+        centerSettingButtonNodeList.add(ColorPaletteButton);
+        centerSettingButtonNodeList.add(ClearButton);
         centerSettingPane.getChildren().add(SingleClickButton);
         centerSettingPane.getChildren().add(BoxChooseButton);
         centerSettingPane.getChildren().add(ColorPaletteButton);
+        centerSettingPane.getChildren().add(ClearButton);
     }
 
     private void showColorPalettePane() {
@@ -349,16 +386,14 @@ public class CenterPane {
         whiteHotButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent e) {
-                boolean flag = YDialog.showConfirmDialog("Change To WhiteHot ...");
-                if (flag){
-                    setButtonClickBackground(colorPaletteButtonList,whiteHotButton);
-                }
+//                boolean flag = YDialog.showConfirmDialog("Change To WhiteHot ...");
+                setButtonClickBackground(colorPaletteButtonList,whiteHotButton);
             }
         });
         FulguriteButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent e) {
-                YDialog.showInformationDialog("Change To Fulgurite ...");
+//                YDialog.showInformationDialog("Change To Fulgurite ...");
                 setButtonClickBackground(colorPaletteButtonList,FulguriteButton);
             }
         });
@@ -383,8 +418,8 @@ public class CenterPane {
         });
     }
 
-    private void setButtonClickBackground(ArrayList<Button> colorPaletteButtonList,Button clickButton) {
-        for(Button button:colorPaletteButtonList){
+    private void setButtonClickBackground(ArrayList<Button> buttonNodeList,Button clickButton) {
+        for(Button button:buttonNodeList){
             if(button == clickButton){
                 button.setBackground(centerSettingButtonClickBackground);
             }else{
@@ -412,12 +447,21 @@ public class CenterPane {
         return button;
     }
 
-    private void resetCenterSetting() {
-        SingleClickButton.setBackground(centerSettingButtonUnclickBackground);
-        BoxChooseButton.setBackground(centerSettingButtonUnclickBackground);
-        ColorPaletteButton.setBackground(centerSettingButtonUnclickBackground);
-        centerSettingFlag = 0;
+    private void resetShowImageDefault(){
+        setButtonClickBackground(centerSettingButtonNodeList,null);
+        centerSettingFlag = CenterSettingSelect.NONE;
+        centerSettingColorPalettePaneAdded = false;
+        dmissColorPalettePane();
+        for (int i =0;i < pointTemperatureNodeList.size();i++){
+            ArrayList pointNodeList = (ArrayList)pointTemperatureNodeList.get(i);
+            showImagePane.getChildren().removeAll(pointNodeList);
+        }
+        showImagePane.getChildren().removeAll(topLine, bottomLine, leftLine, rightLine);
+        showImagePane.getChildren().removeAll(boxTemperatureNodeMax);
+        showImagePane.getChildren().removeAll(boxTemperatureNodeMin);
+        pointTemperatureNodeList.clear();
+        pointTemperatureDataList.clear();
+        boxTemperatureData = null;
     }
-
 
 }
